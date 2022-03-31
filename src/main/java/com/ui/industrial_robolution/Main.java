d0eca1897101c508bbd6059a4c9f1641dda10db0
Vivien Bálint <vivienbalint97@gmail.com>
2022-03-31 09:12:56 +0200

    Initial commit - made menu UI

---
+++ src/main/java/com/ui/industrial_robolution/Main.java
@@ -0,0 +1,61 @@
+package com.ui.industrial_robolution;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+        //****** Layout ******
+
+        GridPane layout = new GridPane();
+        layout.setAlignment(Pos.CENTER);
+        layout.setStyle("-fx-background-color: #343A40");
+
+        //****** Scene ******
+
+        Scene scene = new Scene(layout, 800, 700);
+
+        //****** Title ******
+
+        Label title = new Label("Industrial Robolution\n");
+        layout.add(title, 1, 1);
+        title.setPadding(new Insets(0, 0, 50, 0));
+        title.setFont(Font.font("Agency FB", 50));
+        title.setStyle("-fx-text-fill: #eeeaa9");
+
+        //****** Start Button ******
+
+        Button startBtn = new Button("START");
+        layout.add(startBtn, 1, 2);
+        startBtn.setFont(Font.font("Agency FB", 30));
+
+        startBtn.setStyle("-fx-background-color: #73688b; -fx-text-fill: #eeeaa9");
+        layout.setHalignment(startBtn, HPos.CENTER);
+
+        startBtn.setOnAction(e -> {
+            MenuFX menu = new MenuFX();
+            startBtn.getScene().setRoot(menu.getRootPane());
+        });
+
+
+        primaryStage.setTitle("Industrial Robolution");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/ui/industrial_robolution/MenuFX.java
@@ -0,0 +1,54 @@
+package com.ui.industrial_robolution;
+
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+public class MenuFX {
+
+    private final GridPane rootPane;
+
+    public MenuFX() {
+
+        //****** Layout ******
+
+        rootPane = new GridPane();
+        rootPane.setAlignment(Pos.CENTER);
+        rootPane.setStyle("-fx-background-color: #343A40");
+        rootPane.setHgap(20);
+
+        //****** Title ******
+
+        Label levelLabel = new Label("Choose Your Level:");
+        levelLabel.setFont(Font.font("Agency FB", 50));
+        levelLabel.setStyle("-fx-text-fill: #eeeaa9");
+        levelLabel.setPadding(new Insets(0, 0, 50, 0));
+        rootPane.add(levelLabel, 1, 1);
+        rootPane.setHalignment(levelLabel, HPos.CENTER);
+        rootPane.setColumnSpan(levelLabel, 5);
+
+        //****** Buttons ******
+
+        String[] levels = {"novice", "adept", "expert", "master", "infinite"};
+
+        int levelCount = 0;
+
+        for(String level : levels) {
+            levelCount++;
+            Button levelBtn = new Button();
+            levelBtn.setText(level.toUpperCase());
+            levelBtn.setFont(Font.font("Agency FB", 30));
+            levelBtn.setStyle("-fx-background-color: #73688b; -fx-text-fill: #eeeaa9");
+            rootPane.add(levelBtn, levelCount, 2);
+            rootPane.setHalignment(levelBtn, HPos.CENTER);
+        }
+    }
+
+    public GridPane getRootPane() {
+        return rootPane;
+    }
+}
